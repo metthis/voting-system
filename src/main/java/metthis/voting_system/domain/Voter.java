f4636db16381c9ff718f3c4982f8d48e870b6939
metthis <matyas.vilimek@gmail.com>
2023-09-04 20:45:45 +0300

    add name functionality to Voter

---
+++ src/main/java/metthis/voting_system/domain/Voter.java
@@ -0,0 +1,13 @@
+package metthis.voting_system.domain;
+
+public class Voter {
+    private String name;
+
+    public Voter(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
