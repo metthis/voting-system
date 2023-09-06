0bba2ec1f8f1bbf4798a83b96e9513580f112c57
metthis <matyas.vilimek@gmail.com>
2023-09-06 22:47:54 +0300

    add Election, PresidentialElection, VotingRound, blank

---
+++ src/main/java/metthis/voting_system/logic/elections/Election.java
@@ -0,0 +1,5 @@
+package metthis.voting_system.logic.elections;
+
+public abstract class Election {
+
+}
---
+++ src/main/java/metthis/voting_system/logic/voting/VotingRound.java
@@ -0,0 +1,5 @@
+package metthis.voting_system.logic.voting;
+
+public class VotingRound {
+
+}
