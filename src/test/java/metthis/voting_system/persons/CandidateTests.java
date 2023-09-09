a5cf7aaaddeb9670d39da38ed0212e8a0b002dd2
metthis <matyas.vilimek@gmail.com>
2023-09-09 10:34:00 +0300

    remove empty line in CandidateTests

--- src/test/java/metthis/voting_system/persons/CandidateTests.java
+++ src/test/java/metthis/voting_system/persons/CandidateTests.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
 import org.junit.jupiter.api.BeforeEach;
 
 public class CandidateTests {
