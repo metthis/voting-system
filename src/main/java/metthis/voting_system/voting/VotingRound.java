11d5fc75acf40f2d14dc675805c3135ccf8aa3cc
metthis <matyas.vilimek@gmail.com>
2023-09-10 11:21:56 +0300

    remove reference to Election from VotingRound

--- src/main/java/metthis/voting_system/voting/VotingRound.java
+++ src/main/java/metthis/voting_system/voting/VotingRound.java
@@ -1,11 +1,5 @@
 package metthis.voting_system.voting;
 
-import metthis.voting_system.elections.Election;
-
 public class VotingRound {
-    private Election election;
 
-    public Election getElection() {
-        return this.election;
-    }
 }
