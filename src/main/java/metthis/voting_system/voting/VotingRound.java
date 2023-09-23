edd6e4c2803eee607817ff874ca3fc51465f3be2
metthis <matyas.vilimek@gmail.com>
2023-09-23 15:31:05 +0300

    add functionality to VotingRound, add corresponding tests

--- src/main/java/metthis/voting_system/voting/VotingRound.java
+++ src/main/java/metthis/voting_system/voting/VotingRound.java
@@ -1,5 +1,42 @@
 package metthis.voting_system.voting;
 
-public class VotingRound {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+public class VotingRound {
+    private List<BallotBox> ballotBoxes;
+    private Map<Vote, Integer> votes;
+
+    public VotingRound() {
+        this.ballotBoxes = new ArrayList<>();
+        this.votes = null;
+    }
+
+    public BallotBox createBallotBox() {
+        BallotBox ballotBox = new BallotBox();
+        this.ballotBoxes.add(ballotBox);
+        return ballotBox;
+    }
+
+    public List<BallotBox> getBallotBoxes() {
+        return this.ballotBoxes;
+    }
+
+    // For unit tests only:
+    void setBallotBoxes(List<BallotBox> ballotBoxes) {
+        this.ballotBoxes = ballotBoxes;
+    }
+
+    public void collectVotesFromBallotBoxes() {
+        BallotBox allVotes = new BallotBox();
+        for (BallotBox ballotBox : this.ballotBoxes) {
+            ballotBox.dumpIntoAndKeep(allVotes);
+        }
+        this.votes = allVotes.getVotes();
+    }
+
+    public Map<Vote, Integer> getVotes() {
+        return this.votes;
+    }
 }
---
+++ src/test/java/metthis/voting_system/voting/VotingRoundTests.java
@@ -0,0 +1,149 @@
+package metthis.voting_system.voting;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import metthis.voting_system.persons.Candidate;
+
+public class VotingRoundTests {
+    @Nested
+    class New {
+        @Test
+        void isInstantiatedWithNew() {
+            new VotingRound();
+        }
+    }
+
+    @Nested
+    class WhenNew {
+        private VotingRound votingRound;
+
+        @BeforeEach
+        void initVotingRound() {
+            this.votingRound = new VotingRound();
+        }
+
+        @Test
+        void canCreateBallotBox() {
+            votingRound.createBallotBox();
+        }
+
+        @Test
+        void createBallotBoxReturnsEmptyBallotBox() {
+            BallotBox ballotBox = votingRound.createBallotBox();
+            assertEquals(0, ballotBox.getVotes().size());
+        }
+
+        @Test
+        void canGetBallotBoxes() {
+            votingRound.getBallotBoxes();
+        }
+
+        @Test
+        void creatingBallotBoxOnceAndGettingBallotBoxesReturnsCollectionWithOneBallotBox() {
+            votingRound.createBallotBox();
+            Collection<BallotBox> ballotBoxes = votingRound.getBallotBoxes();
+            assertEquals(1, ballotBoxes.size());
+        }
+
+        @Test
+        void creatingBallotBoxTwiceAndGettingBallotBoxesReturnsCollectionWithTwoBallotBoxes() {
+            votingRound.createBallotBox();
+            votingRound.createBallotBox();
+            Collection<BallotBox> ballotBoxes = votingRound.getBallotBoxes();
+            assertEquals(2, ballotBoxes.size());
+        }
+
+        @Test
+        void canCollectVotesFromBallotBoxes() {
+            votingRound.collectVotesFromBallotBoxes();
+        }
+
+        @Test
+        void canGetVotes() {
+            votingRound.getVotes();
+        }
+
+        @Test
+        void getVotesInitiallyReturnsNull() {
+            assertNull(votingRound.getVotes());
+        }
+
+        @ParameterizedTest
+        @MethodSource
+        @DisplayName("Votes from ballot boxes get correctly collected by collectVotesFromBallotBoxes() and returned by getVotes()")
+        void votesGetCorrectlyCollectedAndReturned(
+                List<BallotBox> ballotBoxes,
+                Map<Vote, Integer> expectedVotes) {
+            votingRound.setBallotBoxes(ballotBoxes);
+            votingRound.collectVotesFromBallotBoxes();
+            assertEquals(expectedVotes, votingRound.getVotes());
+        }
+
+        static Stream<Arguments> votesGetCorrectlyCollectedAndReturned() {
+            Candidate[] candidates = ParametrisedUtil.getDifferenCandidates(3);
+
+            // -1 means "create new ballot box
+            // and add the following votes into it"
+            // i (ie. 0 or more) means "add vote for candidate i"
+            List<List<Integer>> ballotBoxesBlueprints = Arrays.asList(
+                    Arrays.asList(),
+                    Arrays.asList(-1),
+                    Arrays.asList(-1, -1),
+                    Arrays.asList(
+                            -1,
+                            -1, 0, 1, 1, 2),
+                    // Tests that values from different ballot boxes
+                    // under the same key add to each other,
+                    // not replace each other:
+                    Arrays.asList(
+                            -1, 0, 1, 2,
+                            -1, 0, 1, 2),
+                    Arrays.asList(
+                            -1, 0, 2,
+                            -1, 2,
+                            -1, 0, 1,
+                            -1, 1, 1, 2, 2, 2));
+            List<Map<Integer, Integer>> expectedVotesBlueprints = Arrays.asList(
+                    Map.of(),
+                    Map.of(),
+                    Map.of(),
+                    Map.of(
+                            0, 1,
+                            1, 2,
+                            2, 1),
+                    Map.of(
+                            0, 2,
+                            1, 2,
+                            2, 2),
+                    Map.of(
+                            0, 2,
+                            1, 3,
+                            2, 5));
+
+            List<List<BallotBox>> ballotBoxGroups = ParametrisedUtil
+                    .getBallotBoxGroupsFromBlueprints(
+                            ballotBoxesBlueprints, candidates);
+            List<Map<Vote, Integer>> expectedVotesList = ParametrisedUtil
+                    .getVotesListFromBlueprints(
+                            expectedVotesBlueprints, candidates);
+
+            return ParametrisedUtil
+                    .multipleListsToStreamOfArguments(ballotBoxGroups, expectedVotesList);
+        }
+    }
+}
