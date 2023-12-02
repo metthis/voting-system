b4851667541857fff1b9bc46ceb6c493a53ae4f2
metthis <matyas.vilimek@gmail.com>
2023-12-02 18:34:26 +0200

    add the Spring Boot application class VotingSystemApplication

---
+++ src/main/java/metthis/voting_system/programs/VotingSystemApplication.java
@@ -0,0 +1,11 @@
+package metthis.voting_system.programs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VotingSystemApplication.class, args);
+    }
+}
