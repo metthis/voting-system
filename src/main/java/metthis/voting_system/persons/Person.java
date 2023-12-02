6060245eb72dc86684e6ac34ffa19089d9e731cb
metthis <matyas.vilimek@gmail.com>
2023-12-02 17:35:19 +0200

    remove @GeneratedValue from Person.ID because an ID is supposed to be supplied to the constructor

--- src/main/java/metthis/voting_system/persons/Person.java
+++ src/main/java/metthis/voting_system/persons/Person.java
@@ -5,7 +5,6 @@ import java.time.temporal.ChronoUnit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
@@ -14,7 +13,6 @@ import jakarta.persistence.InheritanceType;
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Person implements Comparable<Person> {
     @Id
-    @GeneratedValue
     private String ID;
 
     @Column(nullable = false)
