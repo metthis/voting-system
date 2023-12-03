80b42f5d83b28b242cbeddb19ee5fe412a458d42
metthis <matyas.vilimek@gmail.com>
2023-12-03 16:52:24 +0200

    remove methods from PersonRepositoryCustom and its ...Impl class which have an equivalent PersonRepository method or just aren't needed

--- src/main/java/metthis/voting_system/persons/PersonRepositoryCustom.java
+++ src/main/java/metthis/voting_system/persons/PersonRepositoryCustom.java
@@ -1,30 +1,14 @@
 package metthis.voting_system.persons;
 
-import java.util.Map;
-
 import org.springframework.data.repository.NoRepositoryBean;
 
 import metthis.voting_system.elections.Election;
 
 @NoRepositoryBean
 public interface PersonRepositoryCustom<P extends Person> {
-    P addIfAbsent(P person);
-
-    P update(P person);
-
-    P addOrUpdate(P person);
-
-    P remove(P person);
-
-    void clear();
-
     boolean isEmpty();
 
-    boolean contains(P person);
-
-    Map<String, P> getRegister();
-
-    int howManyRegistered();
+    boolean exists(P person);
 
     int howManyEligible(Election election);
 }
--- src/main/java/metthis/voting_system/persons/PersonRepositoryCustomImpl.java
+++ src/main/java/metthis/voting_system/persons/PersonRepositoryCustomImpl.java
@@ -1,9 +1,5 @@
 package metthis.voting_system.persons;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 
@@ -12,65 +8,11 @@ public abstract class PersonRepositoryCustomImpl<P extends Person> {
     @Lazy
     PersonRepository<P> personRepository;
 
-    private P getPersonById(P person) {
-        Optional<P> optionalPerson = personRepository.findById(person.getID());
-        return optionalPerson.isPresent() ? optionalPerson.get() : null;
-    }
-
-    public P addIfAbsent(P person) {
-        P presentPerson = getPersonById(person);
-        if (presentPerson == null) {
-            personRepository.save(person);
-            return null;
-        }
-        return presentPerson;
-    }
-
-    public P update(P person) {
-        P presentPerson = getPersonById(person);
-        if (presentPerson == null) {
-            return null;
-        }
-        personRepository.save(person);
-        return presentPerson;
-    }
-
-    public P addOrUpdate(P person) {
-        P presentPerson = getPersonById(person);
-        personRepository.save(person);
-        return presentPerson;
-    }
-
-    public P remove(P person) {
-        P presentPerson = getPersonById(person);
-        if (presentPerson == null) {
-            return null;
-        }
-        personRepository.delete(person);
-        return presentPerson;
-    }
-
-    public void clear() {
-        personRepository.deleteAll();
-    }
-
     public boolean isEmpty() {
         return personRepository.count() == 0;
     }
 
-    public boolean contains(P person) {
-        return getPersonById(person) == person;
-    }
-
-    public Map<String, P> getRegister() {
-        Map<String, P> map = new HashMap<>();
-        for (P person : personRepository.findAll()) {
-            map.put(person.getID(), person);
-        }
-        return map;
-    }
-
-    public int howManyRegistered() {
-        return (int) personRepository.count();
+    public boolean exists(P person) {
+        return personRepository.existsById(person.getID());
     }
 }
