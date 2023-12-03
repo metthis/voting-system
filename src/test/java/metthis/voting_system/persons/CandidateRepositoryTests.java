124cff777f5eea0bec5b471f0dc6af4944877781
metthis <matyas.vilimek@gmail.com>
2023-12-03 22:32:35 +0200

    test repositories in a dedicated VotingSystemTests database

--- src/test/java/metthis/voting_system/persons/CandidateRepositoryTests.java
+++ src/test/java/metthis/voting_system/persons/CandidateRepositoryTests.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("/test.properties")
 public class CandidateRepositoryTests {
 
     @Autowired
--- src/test/java/metthis/voting_system/persons/VoterRepositoryTests.java
+++ src/test/java/metthis/voting_system/persons/VoterRepositoryTests.java
@@ -10,8 +10,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("/test.properties")
 public class VoterRepositoryTests {
     @Autowired
     private VoterRepository voterRepository;
