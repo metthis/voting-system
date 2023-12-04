06b85e261d14550bce6cf315488a515c85750c33
metthis <matyas.vilimek@gmail.com>
2023-12-04 08:58:55 +0200

    update CandidateTests and VoterTests to be in line with the new versions of Person, Candidate and Voter

--- src/test/java/metthis/voting_system/persons/CandidateTests.java
+++ src/test/java/metthis/voting_system/persons/CandidateTests.java
@@ -9,157 +9,194 @@ import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-// @Disabled("Migration to Spring Data is in progress")
 public class CandidateTests {
     private Candidate candidate;
 
-    @BeforeEach
-    void initEach() {
-        this.candidate = new Candidate("Jane Doe", "12345678", "1995-05-03", true, "2023-03-15");
+    @Nested
+    class New {
+        @Test
+        void isInitializedWithDefaultConstructor() {
+            new Candidate();
+        }
+
+        @Test
+        void isInitializedWithConstructorWithArguments() {
+            new Candidate("Jane Doe", "12345678", "1995-05-03", true, "2023-03-15");
+        }
     }
 
-    @Test
-    void canGetName() {
-        assertEquals("Jane Doe", this.candidate.getName());
+    @Nested
+    class WhenNew {
+        @BeforeEach
+        void initEach() {
+            candidate = new Candidate("Jane Doe", "12345678", "1995-05-03", true, "2023-03-15");
+        }
+
+        @Test
+        void canGetName() {
+            assertEquals("Jane Doe", candidate.getName());
+        }
+
+        @Test
+        void canGetID() {
+            assertEquals("12345678", candidate.getID());
+        }
+
+        @Test
+        void getDateOfBirthIsInLocalDateFormat() {
+            LocalDate expected = LocalDate.of(1995, 05, 03);
+            assertEquals(expected, candidate.getDateOfBirth());
+        }
+
+        @Test
+        void canGetIsCitizen() {
+            assertEquals(true, candidate.getIsCitizen());
+        }
+
+        @ParameterizedTest
+        @CsvSource(textBlock = """
+                2013-05-02,     17
+                2013-05-03,     18
+                2013-05-04,     18
+                """)
+        void getAgeReturnsCorrectAge(LocalDate date, int expectedAge) {
+            int age = candidate.getAge(date);
+
+            assertEquals(expectedAge, age);
+        }
+
+        @Test
+        void equalsReturnsTrueWhenAllFieldsAreSame() {
+            Candidate candidate1 = new Candidate("name", "IDid456", "2020-01-01", false, "2020-01-01");
+            Candidate candidate2 = new Candidate("name", "IDid456", "2020-01-01", false, "2020-01-01");
+
+            assertTrue(candidate1.equals(candidate2));
+        }
+
+        @Test
+        void equalsReturnsTrueWhenOnlyIDsAreSame() {
+            Candidate candidate1 = new Candidate("name1", "IDid456", "2020-01-01", false, "2021-01-01");
+            Candidate candidate2 = new Candidate("name1", "IDid456", "1920-12-31", true, "1921-12-31");
+
+            assertTrue(candidate1.equals(candidate2));
+        }
+
+        @Test
+        void equalsReturnsFalseWhenIDsAreDifferent() {
+            Candidate candidate1 = new Candidate("name", "IDid456", "2020-01-01", false, "2020-01-01");
+            Candidate candidate2 = new Candidate("name", "foobar", "2020-01-01", false, "2020-01-01");
+
+            assertFalse(candidate1.equals(candidate2));
+        }
+
+        @ParameterizedTest
+        @CsvSource(textBlock = """
+                1,  2,  -1.0
+                2,  1,  1.0
+                1,  1,  0
+                """)
+        void compareToReturnsCorrectlySignedInt(String ID1, String ID2, double expected) {
+            Candidate candidate1 = new Candidate("", ID1, "2000-01-01", false, "2020-01-01");
+            Candidate candidate2 = new Candidate("", ID2, "2000-01-01", false, "2020-01-01");
+
+            double actual = Math.signum((double) candidate1.compareTo(candidate2));
+
+            assertEquals(expected, actual);
+        }
+
+        @ParameterizedTest
+        @CsvSource(textBlock = """
+                1,      1
+                1,      2
+                1,      a
+                a,      b
+                B,      a
+                a,      aa
+                """)
+        void sortingWorksCorrectly(String smallerID, String largerID) {
+            Candidate smallerCandidate = new Candidate("", smallerID, "2000-01-01", false, "2020-01-01");
+            Candidate largerCandidate = new Candidate("", largerID, "2000-01-01", false, "2020-01-01");
+
+            Candidate[] resultedOrder = { smallerCandidate, largerCandidate };
+            Arrays.sort(resultedOrder);
+
+            Candidate[] resultedOrderInputsFlipped = { largerCandidate, smallerCandidate };
+            Arrays.sort(resultedOrderInputsFlipped);
+
+            assertEquals(smallerID, resultedOrder[0].getID(), resultedOrderInputsFlipped[0].getID());
+            assertEquals(largerID, resultedOrder[1].getID(), resultedOrderInputsFlipped[1].getID());
+        }
+
+        // The following are tests specific to Candidate:
+
+        @Test
+        void getRegistrationDateIsInLocalDateFormat() {
+            LocalDate expected = LocalDate.of(2023, 03, 15);
+            assertEquals(expected, candidate.getRegistrationDate());
+        }
+
+        @Test
+        void canGetwithdrawalDate() {
+            candidate.getWithdrawalDate();
+        }
+
+        @Test
+        void withdrawalDateCanBeSetToLocalDateFormat() {
+            candidate.withdraw("2023-10-31");
+            LocalDate expected = LocalDate.of(2023, 10, 31);
+            assertEquals(expected, candidate.getWithdrawalDate());
+        }
+
+        @Test
+        void canGetlostThisElection() {
+            candidate.getLostThisElection();
+        }
+
+        @Test
+        void lostThisElectionCanBeSetToTrue() {
+            candidate.setLostThisElection(true);
+            assertTrue(candidate.getLostThisElection());
+        }
+
+        @Test
+        void lostThisElectionCanBeSetBackToFalse() {
+            candidate.setLostThisElection(true);
+            candidate.setLostThisElection(false);
+            assertFalse(candidate.getLostThisElection());
+        }
     }
 
-    @Test
-    void canGetID() {
-        assertEquals("12345678", this.candidate.getID());
-    }
+    @Nested
+    class DefaultFieldValues {
 
-    @Test
-    void getDateOfBirthIsInLocalDateFormat() {
-        LocalDate expected = LocalDate.of(1995, 05, 03);
-        assertEquals(expected, this.candidate.getDateOfBirth());
-    }
+        @BeforeEach
+        void initWithTheCorrectConstructor(RepetitionInfo repetitionInfo) {
+            int repetition = repetitionInfo.getCurrentRepetition();
+            if (repetition == 1) {
+                candidate = new Candidate();
+            } else if (repetition == 2) {
+                candidate = new Candidate("Jane Doe", "12345678", "1995-05-03", true, "2023-03-15");
+            }
+        }
 
-    @Test
-    void canGetIsCitizen() {
-        assertEquals(true, this.candidate.getIsCitizen());
-    }
+        // The following are tests specific to Candidate:
 
-    @ParameterizedTest
-    @CsvSource(textBlock = """
-            2013-05-02,     17
-            2013-05-03,     18
-            2013-05-04,     18
-            """)
-    void getAgeReturnsCorrectAge(LocalDate date, int expectedAge) {
-        int age = this.candidate.getAge(date);
+        @RepeatedTest(2)
+        void withdrawalDateIsInitiallyNull() {
+            assertNull(candidate.getWithdrawalDate());
+        }
 
-        assertEquals(expectedAge, age);
-    }
+        @RepeatedTest(2)
+        void lostThisElectionIsInitiallyFalse() {
+            assertFalse(candidate.getLostThisElection());
+        }
 
-    @Test
-    void equalsReturnsTrueWhenAllFieldsAreSame() {
-        Candidate candidate1 = new Candidate("name", "IDid456", "2020-01-01", false, "2020-01-01");
-        Candidate candidate2 = new Candidate("name", "IDid456", "2020-01-01", false, "2020-01-01");
-
-        assertTrue(candidate1.equals(candidate2));
-    }
-
-    @Test
-    void equalsReturnsTrueWhenOnlyIDsAreSame() {
-        Candidate candidate1 = new Candidate("name1", "IDid456", "2020-01-01", false, "2021-01-01");
-        Candidate candidate2 = new Candidate("name1", "IDid456", "1920-12-31", true, "1921-12-31");
-
-        assertTrue(candidate1.equals(candidate2));
-    }
-
-    @Test
-    void equalsReturnsFalseWhenIDsAreDifferent() {
-        Candidate candidate1 = new Candidate("name", "IDid456", "2020-01-01", false, "2020-01-01");
-        Candidate candidate2 = new Candidate("name", "foobar", "2020-01-01", false, "2020-01-01");
-
-        assertFalse(candidate1.equals(candidate2));
-    }
-
-    @Test
-    void equalsReturnsTrueWhenBothIDsAreNull() {
-        Candidate candidate1 = new Candidate("name", null, "2020-01-01", false, "2020-01-01");
-        Candidate candidate2 = new Candidate("name", null, "2020-01-01", false, "2020-01-01");
-
-        assertTrue(candidate1.equals(candidate2));
-    }
-
-    @ParameterizedTest
-    @CsvSource(textBlock = """
-            1,  2,  -1.0
-            2,  1,  1.0
-            1,  1,  0
-            """)
-    void compareToReturnsCorrectlySignedInt(String ID1, String ID2, double expected) {
-        Candidate candidate1 = new Candidate("", ID1, "2000-01-01", false, "2020-01-01");
-        Candidate candidate2 = new Candidate("", ID2, "2000-01-01", false, "2020-01-01");
-
-        double actual = Math.signum((double) candidate1.compareTo(candidate2));
-
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvSource(textBlock = """
-            1,      1
-            1,      2
-            1,      a
-            a,      b
-            B,      a
-            a,      aa
-            """)
-    void sortingWorksCorrectly(String smallerID, String largerID) {
-        Candidate smallerCandidate = new Candidate("", smallerID, "2000-01-01", false, "2020-01-01");
-        Candidate largerCandidate = new Candidate("", largerID, "2000-01-01", false, "2020-01-01");
-
-        Candidate[] resultedOrder = { smallerCandidate, largerCandidate };
-        Arrays.sort(resultedOrder);
-
-        Candidate[] resultedOrderInputsFlipped = { largerCandidate, smallerCandidate };
-        Arrays.sort(resultedOrderInputsFlipped);
-
-        assertEquals(smallerID, resultedOrder[0].getID(), resultedOrderInputsFlipped[0].getID());
-        assertEquals(largerID, resultedOrder[1].getID(), resultedOrderInputsFlipped[1].getID());
-    }
-
-    // The following are tests of methods specific to Candidate:
-
-    @Test
-    void getRegistrationDateIsInLocalDateFormat() {
-        LocalDate expected = LocalDate.of(2023, 03, 15);
-        assertEquals(expected, this.candidate.getRegistrationDate());
-    }
-
-    @Test
-    void withdrawalDateIsInitiallyNull() {
-        assertNull(this.candidate.getWithdrawalDate());
-    }
-
-    @Test
-    void withdrawalDateCanBeSetToLocalDateFormat() {
-        this.candidate.withdraw("2023-10-31");
-        LocalDate expected = LocalDate.of(2023, 10, 31);
-        assertEquals(expected, this.candidate.getWithdrawalDate());
-    }
-
-    @Test
-    void lostThisElectionIsInitiallyFalse() {
-        assertFalse(this.candidate.getLostThisElection());
-    }
-
-    @Test
-    void lostThisElectionCanBeSetToTrue() {
-        this.candidate.setLostThisElection(true);
-        assertTrue(this.candidate.getLostThisElection());
-    }
-
-    @Test
-    void lostThisElectionCanBeSetBackToFalse() {
-        this.candidate.setLostThisElection(true);
-        this.candidate.setLostThisElection(false);
-        assertFalse(this.candidate.getLostThisElection());
     }
 }
--- src/test/java/metthis/voting_system/persons/VoterTests.java
+++ src/test/java/metthis/voting_system/persons/VoterTests.java
@@ -8,6 +8,9 @@ import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -16,132 +19,161 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class VoterTests {
     private Voter voter;
 
-    @BeforeEach
-    void initEach() {
-        this.voter = new Voter("Jane Doe", "12345678", "1995-05-03", true);
+    @Nested
+    class New {
+        @Test
+        void isInitializedWithDefaultConstructor() {
+            new Voter();
+        }
+
+        @Test
+        void isInitializedWithConstructorWithArguments() {
+            new Voter("Jane Doe", "12345678", "1995-05-03", true);
+        }
     }
 
-    @Test
-    void canGetName() {
-        assertEquals("Jane Doe", this.voter.getName());
+    @Nested
+    class WhenNew {
+        @BeforeEach
+        void initEach() {
+            voter = new Voter("Jane Doe", "12345678", "1995-05-03", true);
+        }
+
+        @Test
+        void canGetName() {
+            assertEquals("Jane Doe", voter.getName());
+        }
+
+        @Test
+        void canGetID() {
+            assertEquals("12345678", voter.getID());
+        }
+
+        @Test
+        void getDateOfBirthIsInLocalDateFormat() {
+            LocalDate expected = LocalDate.of(1995, 05, 03);
+            assertEquals(expected, voter.getDateOfBirth());
+        }
+
+        @Test
+        void canGetIsCitizen() {
+            assertEquals(true, voter.getIsCitizen());
+        }
+
+        @ParameterizedTest
+        @CsvSource(textBlock = """
+                2013-05-02,     17
+                2013-05-03,     18
+                2013-05-04,     18
+                """)
+        void getAgeReturnsCorrectAge(LocalDate date, int expectedAge) {
+            int age = voter.getAge(date);
+
+            assertEquals(expectedAge, age);
+        }
+
+        @Test
+        void equalsReturnsTrueWhenAllFieldsAreSame() {
+            Voter voter1 = new Voter("name", "IDid456", "2020-01-01", false);
+            Voter voter2 = new Voter("name", "IDid456", "2020-01-01", false);
+
+            assertTrue(voter1.equals(voter2));
+        }
+
+        @Test
+        void equalsReturnsTrueWhenOnlyIDsAreSame() {
+            Voter voter1 = new Voter("name1", "IDid456", "2020-01-01", false);
+            Voter voter2 = new Voter("name1", "IDid456", "1920-12-31", true);
+
+            assertTrue(voter1.equals(voter2));
+        }
+
+        @Test
+        void equalsReturnsFalseWhenIDsAreDifferent() {
+            Voter voter1 = new Voter("name", "IDid456", "2020-01-01", false);
+            Voter voter2 = new Voter("name", "foobar", "2020-01-01", false);
+
+            assertFalse(voter1.equals(voter2));
+        }
+
+        @ParameterizedTest
+        @CsvSource(textBlock = """
+                1,  2,  -1.0
+                2,  1,  1.0
+                1,  1,  0
+                """)
+        void compareToReturnsCorrectlySignedInt(String ID1, String ID2, double expected) {
+            Voter voter1 = new Voter("", ID1, "2000-01-01", false);
+            Voter voter2 = new Voter("", ID2, "2000-01-01", false);
+
+            double actual = Math.signum((double) voter1.compareTo(voter2));
+
+            assertEquals(expected, actual);
+        }
+
+        @ParameterizedTest
+        @CsvSource(textBlock = """
+                1,      1
+                1,      2
+                1,      a
+                a,      b
+                B,      a
+                a,      aa
+                """)
+        void sortingWorksCorrectly(String smallerID, String largerID) {
+            Voter smallerVoter = new Voter("", smallerID, "2000-01-01", false);
+            Voter largerVoter = new Voter("", largerID, "2000-01-01", false);
+
+            Voter[] resultedOrder = { smallerVoter, largerVoter };
+            Arrays.sort(resultedOrder);
+
+            Voter[] resultedOrderInputsFlipped = { largerVoter, smallerVoter };
+            Arrays.sort(resultedOrderInputsFlipped);
+
+            assertEquals(smallerID, resultedOrder[0].getID(), resultedOrderInputsFlipped[0].getID());
+            assertEquals(largerID, resultedOrder[1].getID(), resultedOrderInputsFlipped[1].getID());
+        }
+
+        // The following are tests pecific to Voter:
+
+        @Test
+        void canGetLastVotedRound() {
+            voter.getLastVotedRound();
+        }
+
+        @Test
+        void canSetLastVotedRound() {
+            voter.setLastVotedRound(1);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = { -10, -1, 0, 1, 2, 3, 4, 5, 10, 20, 137, 12345 })
+        void getLastVotedRoundReturnsValueToWhichItWasSet(int value) {
+            voter.setLastVotedRound(value);
+            int actual = voter.getLastVotedRound();
+            assertEquals(value, actual);
+        }
+
     }
 
-    @Test
-    void canGetID() {
-        assertEquals("12345678", this.voter.getID());
+    @Nested
+    class DefaultFieldValues {
+
+        @BeforeEach
+        void initWithTheCorrectConstructor(RepetitionInfo repetitionInfo) {
+            int repetition = repetitionInfo.getCurrentRepetition();
+            if (repetition == 1) {
+                voter = new Voter();
+            } else if (repetition == 2) {
+                voter = new Voter("Jane Doe", "12345678", "1995-05-03", true);
+            }
+        }
+
+        // The following are tests specific to Candidate:
+
+        @RepeatedTest(2)
+        void getLastVotedRoundInitiallyReturns0() {
+            assertEquals(0, voter.getLastVotedRound());
+        }
     }
-
-    @Test
-    void getDateOfBirthIsInLocalDateFormat() {
-        LocalDate expected = LocalDate.of(1995, 05, 03);
-        assertEquals(expected, this.voter.getDateOfBirth());
-    }
-
-    @Test
-    void canGetIsCitizen() {
-        assertEquals(true, this.voter.getIsCitizen());
-    }
-
-    @ParameterizedTest
-    @CsvSource(textBlock = """
-            2013-05-02,     17
-            2013-05-03,     18
-            2013-05-04,     18
-            """)
-    void getAgeReturnsCorrectAge(LocalDate date, int expectedAge) {
-        int age = this.voter.getAge(date);
-
-        assertEquals(expectedAge, age);
-    }
-
-    @Test
-    void equalsReturnsTrueWhenAllFieldsAreSame() {
-        Voter voter1 = new Voter("name", "IDid456", "2020-01-01", false);
-        Voter voter2 = new Voter("name", "IDid456", "2020-01-01", false);
-
-        assertTrue(voter1.equals(voter2));
-    }
-
-    @Test
-    void equalsReturnsTrueWhenOnlyIDsAreSame() {
-        Voter voter1 = new Voter("name1", "IDid456", "2020-01-01", false);
-        Voter voter2 = new Voter("name1", "IDid456", "1920-12-31", true);
-
-        assertTrue(voter1.equals(voter2));
-    }
-
-    @Test
-    void equalsReturnsFalseWhenIDsAreDifferent() {
-        Voter voter1 = new Voter("name", "IDid456", "2020-01-01", false);
-        Voter voter2 = new Voter("name", "foobar", "2020-01-01", false);
-
-        assertFalse(voter1.equals(voter2));
-    }
-
-    @Test
-    void equalsReturnsTrueWhenBothIDsAreNull() {
-        Voter voter1 = new Voter("name", null, "2020-01-01", false);
-        Voter voter2 = new Voter("name", null, "2020-01-01", false);
-
-        assertTrue(voter1.equals(voter2));
-    }
-
-    @ParameterizedTest
-    @CsvSource(textBlock = """
-            1,  2,  -1.0
-            2,  1,  1.0
-            1,  1,  0
-            """)
-    void compareToReturnsCorrectlySignedInt(String ID1, String ID2, double expected) {
-        Voter voter1 = new Voter("", ID1, "2000-01-01", false);
-        Voter voter2 = new Voter("", ID2, "2000-01-01", false);
-
-        double actual = Math.signum((double) voter1.compareTo(voter2));
-
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvSource(textBlock = """
-            1,      1
-            1,      2
-            1,      a
-            a,      b
-            B,      a
-            a,      aa
-            """)
-    void sortingWorksCorrectly(String smallerID, String largerID) {
-        Voter smallerVoter = new Voter("", smallerID, "2000-01-01", false);
-        Voter largerVoter = new Voter("", largerID, "2000-01-01", false);
-
-        Voter[] resultedOrder = { smallerVoter, largerVoter };
-        Arrays.sort(resultedOrder);
-
-        Voter[] resultedOrderInputsFlipped = { largerVoter, smallerVoter };
-        Arrays.sort(resultedOrderInputsFlipped);
-
-        assertEquals(smallerID, resultedOrder[0].getID(), resultedOrderInputsFlipped[0].getID());
-        assertEquals(largerID, resultedOrder[1].getID(), resultedOrderInputsFlipped[1].getID());
-    }
-
-    // The following are tests of methods specific to Voter:
-
-    @Test
-    void getLastVotedRoundInitiallyReturns0() {
-        assertEquals(0, this.voter.getLastVotedRound());
-    }
-
-    @Test
-    void canSetLastVotedRound() {
-        this.voter.setLastVotedRound(1);
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = { -10, -1, 0, 1, 2, 3, 4, 5, 10, 20, 137, 12345 })
-    void getLastVotedRoundReturnsValueToWhichItWasSet(int value) {
-        this.voter.setLastVotedRound(value);
-        int actual = this.voter.getLastVotedRound();
-        assertEquals(value, actual);
-    }
-
 }
