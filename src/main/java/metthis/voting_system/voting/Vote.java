02abeda4cad964df305186a45298c6321d2cd4b9
metthis <matyas.vilimek@gmail.com>
2023-12-07 08:46:26 +0200

    add getVotingRound to the Vote interface

--- src/main/java/metthis/voting_system/voting/Vote.java
+++ src/main/java/metthis/voting_system/voting/Vote.java
@@ -5,6 +5,8 @@ import metthis.voting_system.persons.Candidate;
 public interface Vote {
     public Candidate getChoice();
 
+    public Integer getVotingRound();
+
     public int hashCode();
 
     public boolean equals(Object obj);
