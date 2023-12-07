59ffac90030fad67b2d337a77b28ccb548831743
metthis <matyas.vilimek@gmail.com>
2023-12-07 08:45:26 +0200

    add a votingRound field to SingleCandidateSingleChoiceVote and adjust other methods and tests accordingly

--- src/main/java/metthis/voting_system/voting/SingleCandidateSingleChoiceVote.java
+++ src/main/java/metthis/voting_system/voting/SingleCandidateSingleChoiceVote.java
@@ -2,6 +2,7 @@ package metthis.voting_system.voting;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -15,15 +16,31 @@ public class SingleCandidateSingleChoiceVote implements Vote {
     @GeneratedValue
     private UUID id;
 
+    @Column(nullable = false)
+    private Integer votingRound;
+
     @ManyToOne
     private Candidate choice;
 
+    public SingleCandidateSingleChoiceVote(Integer votingRound, Candidate choice) {
+        this.votingRound = votingRound;
+        this.choice = choice;
+    }
+
+    public SingleCandidateSingleChoiceVote(Integer votingRound) {
+        this.votingRound = votingRound;
+    }
+
+    // To be removed when the old implementation is removed
     public SingleCandidateSingleChoiceVote(Candidate choice) {
         this.choice = choice;
     }
 
     public SingleCandidateSingleChoiceVote() {
-        this.choice = null;
+    }
+
+    public Integer getVotingRound() {
+        return this.votingRound;
     }
 
     public Candidate getChoice() {
@@ -34,6 +51,7 @@ public class SingleCandidateSingleChoiceVote implements Vote {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
+        result = prime * result + ((votingRound == null) ? 0 : votingRound.hashCode());
         result = prime * result + ((choice == null) ? 0 : choice.hashCode());
         return result;
     }
@@ -47,6 +65,11 @@ public class SingleCandidateSingleChoiceVote implements Vote {
         if (getClass() != obj.getClass())
             return false;
         SingleCandidateSingleChoiceVote other = (SingleCandidateSingleChoiceVote) obj;
+        if (votingRound == null) {
+            if (other.votingRound != null)
+                return false;
+        } else if (!votingRound.equals(other.votingRound))
+            return false;
         if (choice == null) {
             if (other.choice != null)
                 return false;
--- src/test/java/metthis/voting_system/voting/SingleCandidateSingleChoiceVoteTests.java
+++ src/test/java/metthis/voting_system/voting/SingleCandidateSingleChoiceVoteTests.java
@@ -1,6 +1,7 @@
 package metthis.voting_system.voting;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +12,25 @@ import metthis.voting_system.persons.Candidate;
 
 public class SingleCandidateSingleChoiceVoteTests {
     @Nested
+    class New {
+        @Test
+        void isInitializedWithDefaultConstructor() {
+            new SingleCandidateSingleChoiceVote();
+        }
+
+        @Test
+        void isInitializedWithConstructorWithVotingRoundArgument() {
+            new SingleCandidateSingleChoiceVote(1);
+        }
+
+        @Test
+        void isInitializedWithConstructorWithVotingRoundAndChoiceArguments() {
+            Candidate candidate = new Candidate("name", "ID", "1960-01-01", true, "2020-01-01");
+            new SingleCandidateSingleChoiceVote(1, candidate);
+        }
+    }
+
+    @Nested
     class TestsWhichUseDefaultCandidate {
         private Candidate defaulCandidate;
 
@@ -20,36 +40,35 @@ public class SingleCandidateSingleChoiceVoteTests {
         }
 
         @Test
+        void canGetVotingRound() {
+            SingleCandidateSingleChoiceVote vote = new SingleCandidateSingleChoiceVote(
+                    1,
+                    this.defaulCandidate);
+            Integer actual = vote.getVotingRound();
+            assertEquals(1, actual);
+        }
+
+        @Test
         void canGetChoice() {
             SingleCandidateSingleChoiceVote vote = new SingleCandidateSingleChoiceVote(
+                    1,
                     this.defaulCandidate);
             Candidate actual = vote.getChoice();
             assertEquals(this.defaulCandidate, actual);
         }
 
         @Test
-        void getChoiceAfterParameterlessConstructorReturnsNull() {
-            SingleCandidateSingleChoiceVote vote = new SingleCandidateSingleChoiceVote();
+        void getChoiceAfterConstructorWithOnlyTheVotingRoundArgumentReturnsNull() {
+            SingleCandidateSingleChoiceVote vote = new SingleCandidateSingleChoiceVote(1);
             Candidate actual = vote.getChoice();
             assertNull(actual);
-        }
-
-        @Test
-        void votesWithSameChoiceAreEqualAndHaveSameHashCode() {
-            SingleCandidateSingleChoiceVote vote1 = new SingleCandidateSingleChoiceVote(
-                    this.defaulCandidate);
-            SingleCandidateSingleChoiceVote vote2 = new SingleCandidateSingleChoiceVote(
-                    this.defaulCandidate);
-
-            assertEquals(vote1, vote2);
-            assertEquals(vote1.hashCode(), vote2.hashCode());
         }
     }
 
     @Nested
     class EqualityAndHashCodeTests {
         @Test
-        void votesWithEqualChoiceAreEqualAndHaveSameHashCode() {
+        void votesWithEqualVotingRoundAndEqualChoiceAreEqualAndHaveSameHashCode() {
             Candidate candidate1 = new Candidate("name", "ID", "1960-01-01", true, "2020-01-01");
             Candidate candidate2 = new Candidate("name", "ID", "1960-01-01", true, "2020-01-01");
 
@@ -57,20 +76,50 @@ public class SingleCandidateSingleChoiceVoteTests {
             // for this test's premise to be true
             assertEquals(candidate1, candidate2);
 
-            SingleCandidateSingleChoiceVote vote1 = new SingleCandidateSingleChoiceVote(candidate1);
-            SingleCandidateSingleChoiceVote vote2 = new SingleCandidateSingleChoiceVote(candidate2);
+            SingleCandidateSingleChoiceVote vote1 = new SingleCandidateSingleChoiceVote(1, candidate1);
+            SingleCandidateSingleChoiceVote vote2 = new SingleCandidateSingleChoiceVote(1, candidate2);
 
             assertEquals(vote1, vote2);
             assertEquals(vote1.hashCode(), vote2.hashCode());
         }
 
         @Test
-        void votesWithNullChoiceAreEqualAndHaveSameHashCode() {
+        void votesWithEqualVotingRoundAndNullChoiceAreEqualAndHaveSameHashCode() {
             SingleCandidateSingleChoiceVote vote1 = new SingleCandidateSingleChoiceVote();
             SingleCandidateSingleChoiceVote vote2 = new SingleCandidateSingleChoiceVote();
 
             assertEquals(vote1, vote2);
             assertEquals(vote1.hashCode(), vote2.hashCode());
+        }
+
+        @Test
+        void votesWithDifferentVotingRoundAreNotEqual() {
+            Candidate candidate1 = new Candidate("name", "ID", "1960-01-01", true, "2020-01-01");
+            Candidate candidate2 = new Candidate("name", "ID", "1960-01-01", true, "2020-01-01");
+
+            // These 2 candidates must be considered equal
+            // to isolate the influence of votingRound
+            assertEquals(candidate1, candidate2);
+
+            SingleCandidateSingleChoiceVote vote1 = new SingleCandidateSingleChoiceVote(1, candidate1);
+            SingleCandidateSingleChoiceVote vote2 = new SingleCandidateSingleChoiceVote(2, candidate2);
+
+            assertNotEquals(vote1, vote2);
+        }
+
+        @Test
+        void votesWithDifferentChoiceAreNotEqual() {
+            Candidate candidate1 = new Candidate("name1", "ID1", "1960-01-01", true, "2020-01-01");
+            Candidate candidate2 = new Candidate("name2", "ID2", "1980-01-01", false, "2022-01-01");
+
+            // These 2 candidates must be considered not equal
+            // for this test's premise to be true
+            assertNotEquals(candidate1, candidate2);
+
+            SingleCandidateSingleChoiceVote vote1 = new SingleCandidateSingleChoiceVote(1, candidate1);
+            SingleCandidateSingleChoiceVote vote2 = new SingleCandidateSingleChoiceVote(1, candidate2);
+
+            assertNotEquals(vote1, vote2);
         }
     }
 }
