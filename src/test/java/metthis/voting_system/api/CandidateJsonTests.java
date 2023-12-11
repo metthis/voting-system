add4ca85846a668899ce713338777006c866a6df
metthis <matyas.vilimek@gmail.com>
2023-12-11 22:09:36 +0200

    finish serialization and deserialization tests in CandidateJsonTests, add candidateList.json

--- src/test/java/metthis/voting_system/api/CandidateJsonTests.java
+++ src/test/java/metthis/voting_system/api/CandidateJsonTests.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.boot.test.json.JsonContent;
 
 import metthis.voting_system.persons.Candidate;
 
@@ -30,12 +29,33 @@ public class CandidateJsonTests {
         candidates = Arrays.array(
                 new Candidate("John Smith", "123789", "1960-06-30", true, "2023-12-01"),
                 new Candidate("Jane Doe", "7878002", "1972-01-01", false, "2023-12-02"));
+        candidates[1].setWithdrawalDate("2023-12-10");
+        candidates[1].setLostThisElection(true);
+    }
+
+    byte[] getBytesFromFile(String fileName) throws IOException {
+        return this.getClass().getResourceAsStream(fileName).readAllBytes();
     }
 
     @Test
     void candidateIsSerializedCorrectly() throws IOException {
-        JsonContent<Candidate> candidateJson = json.write(candidates[0]);
-        assertThat(candidateJson).isStrictlyEqualToJson("candidate.json");
+        assertThat(json.write(candidates[0])).isStrictlyEqualToJson("candidate.json");
     }
 
+    @Test
+    void candidateIsDesirializedCorrectly() throws IOException {
+        byte[] actualBytes = getBytesFromFile("candidate.json");
+        assertThat(json.parse(actualBytes)).isEqualTo(candidates[0]);
+    }
+
+    @Test
+    void candidateListIsSerializedCorrectly() throws IOException {
+        assertThat(jsonList.write(candidates)).isStrictlyEqualToJson("candidateList.json");
+    }
+
+    @Test
+    void candidateListIsDesirializedCorrectly() throws IOException {
+        byte[] actualBytes = getBytesFromFile("candidateList.json");
+        assertThat(jsonList.parse(actualBytes)).isEqualTo(candidates);
+    }
 }
