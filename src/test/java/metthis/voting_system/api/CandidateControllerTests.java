f8e5564a9cf376c1ba512917a591db87a1d5e051
metthis <matyas.vilimek@gmail.com>
2023-12-18 21:49:47 +0200

    add a test of GET on /candidates

--- src/test/java/metthis/voting_system/api/CandidateControllerTests.java
+++ src/test/java/metthis/voting_system/api/CandidateControllerTests.java
@@ -1,7 +1,5 @@
 package metthis.voting_system.api;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
@@ -10,6 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = FillRepositories.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CandidateControllerTests {
@@ -55,5 +57,39 @@ public class CandidateControllerTests {
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         assertThat(response.getBody()).isEqualTo("Could not find candidate 999");
+    }
+
+    @Test
+    void getResponds200AndAllCandidatesWhenTheRootIsCalled() {
+        ResponseEntity<String> response = restTemplate
+                .getForEntity("/candidates", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+        int candidateCount = documentContext.read("$.length()");
+        assertThat(candidateCount).isEqualTo(3);
+
+        List<String> ids = documentContext.read("$..id");
+        assertThat(ids).containsExactlyInAnyOrder("140", "abc789", "anID");
+
+        List<String> names = documentContext.read("$..name");
+        assertThat(names).containsExactlyInAnyOrder("Chris Wool", "Mary Given", "Elizabeth Wong");
+
+        List<String> datesOfBirth = documentContext.read("$..dateOfBirth");
+        assertThat(datesOfBirth).containsExactlyInAnyOrder("1985-03-15", "1991-09-09", "2001-11-30");
+
+        List<Boolean> isCitizens = documentContext.read("$..isCitizen");
+        assertThat(isCitizens).containsExactlyInAnyOrder(true, false, true);
+
+        List<String> registrationDates = documentContext.read("$..registrationDate");
+        assertThat(registrationDates).containsExactlyInAnyOrder("2023-11-15", "2023-11-30", "2023-12-05");
+
+        List<String> withdrawalDates = documentContext.read("$..withdrawalDate");
+        assertThat(withdrawalDates).containsExactlyInAnyOrder(null, "2023-12-02", null);
+
+        List<Boolean> lostThisElections = documentContext.read("$..lostThisElection");
+        assertThat(lostThisElections).containsExactlyInAnyOrder(false, false, true);
     }
 }
--- src/test/java/metthis/voting_system/api/FillRepositories.java
+++ src/test/java/metthis/voting_system/api/FillRepositories.java
@@ -2,7 +2,6 @@ package metthis.voting_system.api;
 
 import metthis.voting_system.persons.Candidate;
 import metthis.voting_system.persons.CandidateRepository;
-import org.assertj.core.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -16,11 +15,18 @@ public class FillRepositories {
 
     private static final Candidate[] candidates = {
             new Candidate("Chris Wool", "140",
-                    "1985-03-15", true, "2023-11-15")
+                    "1985-03-15", true, "2023-11-15"),
+            new Candidate("Mary Given", "abc789",
+                          "1991-09-09", false, "2023-11-30"),
+            new Candidate("Elizabeth Wong", "anID",
+                          "2001-11-30", true, "2023-12-05")
     };
 
     @Bean
     CommandLineRunner fillCandidateRepository(CandidateRepository candidateRepository) {
+        candidates[1].setWithdrawalDate("2023-12-02");
+        candidates[2].setLostThisElection(true);
+
         return args -> {
             for (Candidate candidate : candidates) {
                 log.info("Preloading " + candidateRepository.save(candidate));
