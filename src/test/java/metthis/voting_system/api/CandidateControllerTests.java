24c950aaaa74b5952770a87deb8a017827480544
metthis <matyas.vilimek@gmail.com>
2024-01-10 19:52:13 +0200

    refactor CandidateControllerTests by using MockMvc instead

--- src/test/java/metthis/voting_system/api/CandidateControllerTests.java
+++ src/test/java/metthis/voting_system/api/CandidateControllerTests.java
@@ -1,36 +1,34 @@
 package metthis.voting_system.api;
 
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
 import metthis.voting_system.persons.Candidate;
 import metthis.voting_system.persons.CandidateRepository;
 import org.assertj.core.util.Arrays;
-import org.json.JSONException;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.*;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import java.io.IOException;
 import java.net.URI;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 @TestPropertySource("/test.properties")
 // TestInstance.Lifecycle.PER_CLASS is selected to allow the use of @AfterAll on a non-static repository
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CandidateControllerTests {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Autowired
     private CandidateRepository candidateRepository;
@@ -73,108 +71,83 @@ public class CandidateControllerTests {
     }
 
     @Test
-    void getResponds200AndACandidateWhenItExists() throws IOException, JSONException {
-        ResponseEntity<String> response = restTemplate
-                .getForEntity("/candidates/140", String.class);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-
+    void getResponds200AndACandidateWhenItExists() throws Exception {
         String expectedJson = testUtils.fileToString("candidate1.json");
-        JSONAssert.assertEquals(expectedJson, response.getBody(), true);
+
+        mockMvc.perform(get("/candidates/{id}", "140"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedJson, true));
     }
 
     @Test
-    void getResponds404WhenCandidateDoesNotExist() {
-        ResponseEntity<String> response = restTemplate
-                .getForEntity("/candidates/9999", String.class);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-        assertThat(response.getBody()).isEqualTo("Could not find candidate 9999");
+    void getResponds404WhenCandidateDoesNotExist() throws Exception {
+        mockMvc.perform(get("/candidates/{id}", "9999"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+                .andExpect(content().string("Could not find candidate 9999"));
     }
 
     @Test
-    void getResponds200AndWithAllCandidatesWhenTheRootIsCalled() throws  IOException, JSONException {
-        ResponseEntity<String> response = restTemplate
-                .getForEntity("/candidates", String.class);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-
+    void getResponds200AndWithAllCandidatesWhenTheRootIsCalled() throws  Exception {
         String expectedJson = testUtils.fileToString("candidateList2.json");
-        JSONAssert.assertEquals(expectedJson, response.getBody(), true);
+
+        mockMvc.perform(get("/candidates"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedJson, true));
     }
 
-    @ParameterizedTest
-    @NullSource
-    @ValueSource(strings = {"newId99"})
-    void putResponds201AndWithANewCandidateWhenSupplyingANonexistentCandidate(String idInBody)
-            throws IOException, JSONException {
-        Candidate newCandidate = new Candidate("Tiina Glass",
-                                               idInBody,
-                                               "1990-01-01",
-                                               true,
-                                               "2023-12-01");
-        newCandidate.setLostThisElection(true);
-        newCandidate.setWithdrawalDate("2023-12-15");
+    @RepeatedTest(2)
+    void putResponds201AndWithANewCandidateWhenSupplyingANonexistentCandidate(RepetitionInfo repetitionInfo)
+            throws Exception {
+        int currentRepetition = repetitionInfo.getCurrentRepetition();
+        String jsonWithId = testUtils.fileToString("candidateNewWithId.json");
+        String jsonWithoutId = testUtils.fileToString("candidateNewWithoutId.json");
 
-        HttpEntity<Candidate> request = new HttpEntity<>(newCandidate);
-        ResponseEntity<String> createResponse = restTemplate
-                .exchange("/candidates/newId99", HttpMethod.PUT, request, String.class);
+        MvcResult putResponse = mockMvc.perform(put("/candidates/{id}", "newId99")
+                                .header("Content-Type", "application/json")
+                                .content(currentRepetition == 1 ? jsonWithId : jsonWithoutId))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(jsonWithId, true))
+                .andReturn();
 
-        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        URI location = URI.create(putResponse.getResponse().getHeader("Location"));
 
-        URI locationOfNewCandidate = createResponse.getHeaders().getLocation();
-        ResponseEntity<String> getResponse = restTemplate
-                .getForEntity(locationOfNewCandidate, String.class);
-
-        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-        assertThat(createResponse.getBody()).isEqualTo(getResponse.getBody());
-
-        String expectedJson = testUtils.fileToString("candidateNew.json");
-        JSONAssert.assertEquals(expectedJson, createResponse.getBody(), true);
+        mockMvc.perform(get(location))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(jsonWithId, true));
     }
 
-    @ParameterizedTest
-    @NullSource
-    @ValueSource(strings = {"140"})
-    void putResponds204AndUpdatedTheCandidateWhenSupplyingAnAlteredExistingCandidate(String idInBody)
-            throws IOException, JSONException {
-        Candidate updatedCandidate = new Candidate("Tris Bool",
-                                               idInBody,
-                                               "1985-03-15",
-                                               false,
-                                               "2023-11-15");
-        updatedCandidate.setLostThisElection(true);
-        updatedCandidate.setWithdrawalDate("2023-12-15");
+    @RepeatedTest(2)
+    void putResponds204AndUpdatedTheCandidateWhenSupplyingAnAlteredExistingCandidate(RepetitionInfo repetitionInfo)
+            throws Exception {
+        int currentRepetition = repetitionInfo.getCurrentRepetition();
+        String jsonWithId = testUtils.fileToString("candidateUpdatedPutWithId.json");
+        String jsonWithoutId = testUtils.fileToString("candidateUpdatedPutWithoutId.json");
 
-        HttpEntity<Candidate> request = new HttpEntity<>(updatedCandidate);
-        ResponseEntity<String> updateResponse = restTemplate
-                .exchange("/candidates/140", HttpMethod.PUT, request, String.class);
+        mockMvc.perform(put("/candidates/{id}", "140")
+                                                        .header("Content-Type", "application/json")
+                                                        .content(currentRepetition == 1 ? jsonWithId : jsonWithoutId))
+                .andExpect(status().isNoContent());
 
-        assertThat(updateResponse.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-
-        ResponseEntity<String> getResponse = restTemplate
-                .getForEntity("/candidates/140", String.class);
-
-        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-        String expectedJson = testUtils.fileToString("candidateUpdatedPut.json");
-        JSONAssert.assertEquals(expectedJson, getResponse.getBody(), true);
+        mockMvc.perform(get("/candidates/{id}", "140"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(jsonWithId, true));
     }
 
 
     @Test
-    void putResponds400WhenSupplyingInvalidCandidateData() throws IOException {
+    void putResponds400WhenSupplyingInvalidCandidateData() throws Exception {
         String invalidCandidate = testUtils.fileToString("candidateInvalid.json");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-
-        HttpEntity<String> request = new HttpEntity<>(invalidCandidate, headers);
-        ResponseEntity<String> updateResponse = restTemplate
-                .exchange("/candidates/140", HttpMethod.PUT, request, String.class);
-
-        assertThat(updateResponse.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        mockMvc.perform(put("/candidates/{id}", "140")
+                                .header("Content-Type", "application/json")
+                                .content(invalidCandidate))
+                .andExpect(status().isBadRequest());
     }
 
     @ParameterizedTest
@@ -186,97 +159,76 @@ public class CandidateControllerTests {
     void patchResponds200AndWithUpdatedCandidateWhenAnExistingCandidateIsSuccessfullyUpdatedWithWithdrawalDateOrLostThisElectionOrBoth(
             String newWithdrawalDate,
             Boolean newLostThisElection
-    ) throws IOException, JSONException {
-        Candidate updateData = new Candidate();
-        updateData.setWithdrawalDate(newWithdrawalDate);
-        updateData.setLostThisElection(newLostThisElection);
+    ) throws Exception {
+        String expectedWithdrawalDate = newWithdrawalDate;
+        Boolean expectedLostThisElection = newLostThisElection == null ? false : newLostThisElection;
 
-        HttpEntity<Candidate> request = new HttpEntity<>(updateData);
-        ResponseEntity<String> patchResponse = restTemplate
-                .exchange("/candidates/140", HttpMethod.PATCH, request, String.class);
+        String updateJson = testUtils.fileToString("candidateUpdate.json");
+        updateJson = testUtils.modifyJsonField(updateJson, "withdrawalDate", newWithdrawalDate);
+        updateJson = testUtils.modifyJsonField(updateJson, "lostThisElection", newLostThisElection);
 
-        assertThat(patchResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        String expectedJson = testUtils.fileToString("candidateUpdatedPatchPartial.json");
+        expectedJson = testUtils.modifyJsonField(expectedJson, "withdrawalDate", expectedWithdrawalDate);
+        expectedJson = testUtils.modifyJsonField(expectedJson, "lostThisElection", expectedLostThisElection);
 
-        ResponseEntity<String> getResponse = restTemplate
-                .getForEntity("/candidates/140", String.class);
+        mockMvc.perform(patch("/candidates/{id}", "140")
+                                .header("Content-Type", "application/json")
+                                .content(updateJson))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedJson, true));
 
-        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-        assertThat(patchResponse.getBody()).isEqualTo(getResponse.getBody());
-
-        String expectedSubsetJson = testUtils.fileToString("candidateUpdatedPatchPartial.json");
-        String actualJson = patchResponse.getBody();
-
-        JSONAssert.assertEquals(expectedSubsetJson, actualJson, false);
-
-        DocumentContext documentContext = JsonPath.parse(actualJson);
-
-        String withdrawalDate = documentContext.read("$.withdrawalDate");
-        assertThat(withdrawalDate).isEqualTo(newWithdrawalDate);
-
-        Boolean lostThisElection = documentContext.read("$.lostThisElection");
-        assertThat(lostThisElection).isEqualTo(newLostThisElection == null ? false : newLostThisElection);
+        mockMvc.perform(get("/candidates/{id}", "140"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedJson, true));
     }
 
     @Test
-    void patchIgnoresAllUpdateFieldsOtherThanWithdrawalDateAndLostThisElection() throws IOException, JSONException {
-        Candidate updateData = new Candidate("newName", "newId", "1800-01-01",
-                                             false, "2020-01-01");
-
-        updateData.setWithdrawalDate("2023-12-30");
-        updateData.setLostThisElection(true);
-
-        HttpEntity<Candidate> request = new HttpEntity<>(updateData);
-        ResponseEntity<String> patchResponse = restTemplate
-                .exchange("/candidates/140", HttpMethod.PATCH, request, String.class);
-
-        assertThat(patchResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-        ResponseEntity<String> getResponse = restTemplate
-                .getForEntity("/candidates/140", String.class);
-
-        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-        assertThat(patchResponse.getBody()).isEqualTo(getResponse.getBody());
-
+    void patchIgnoresAllUpdateFieldsOtherThanWithdrawalDateAndLostThisElection() throws Exception {
+        String updateJson = testUtils.fileToString("candidateUpdateMostlyIgnored.json");
         String expectedJson = testUtils.fileToString("candidateUpdatedPatchFull.json");
-        JSONAssert.assertEquals(expectedJson, patchResponse.getBody(), true);
+
+        mockMvc.perform(patch("/candidates/{id}", "140")
+                                .header("Content-Type", "application/json")
+                                .content(updateJson))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedJson, true));
+
+        mockMvc.perform(get("/candidates/{id}", "140"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedJson, true));
     }
 
     @Test
-    void patchIgnoresFieldsWhichDoNotCorrespondToCandidateFields() throws IOException, JSONException {
-        String invalidUpdate = testUtils.fileToString("invalidUpdate.json");
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-
-        HttpEntity<String> request = new HttpEntity<>(invalidUpdate, headers);
-        ResponseEntity<String> patchResponse = restTemplate
-                .exchange("/candidates/140", HttpMethod.PATCH, request, String.class);
-
-        assertThat(patchResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-        ResponseEntity<String> getResponse = restTemplate
-                .getForEntity("/candidates/140", String.class);
-
-        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-        assertThat(patchResponse.getBody()).isEqualTo(getResponse.getBody());
-
+    void patchIgnoresFieldsWhichDoNotCorrespondToCandidateFields() throws Exception {
+        String updateJson = testUtils.fileToString("candidateUpdateInvalid.json");
         String expectedJson = testUtils.fileToString("candidate1.json");
-        JSONAssert.assertEquals(expectedJson, patchResponse.getBody(), true);
+
+        mockMvc.perform(patch("/candidates/{id}", "140")
+                                .header("Content-Type", "application/json")
+                                .content(updateJson))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedJson, true));
+
+        mockMvc.perform(get("/candidates/{id}", "140"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedJson, true));
     }
 
     @Test
-    void patchResponds404WhenAttemptingToUpdateANonexistentCandidate() {
-        Candidate updateData = new Candidate();
-        updateData.setWithdrawalDate("2023-12-30");
+    void patchResponds404WhenAttemptingToUpdateANonexistentCandidate() throws Exception {
+        String updateJson = testUtils.fileToString("candidateUpdateInvalid.json");
 
-        HttpEntity<Candidate> request = new HttpEntity<>(updateData);
-        ResponseEntity<String> updateResponse = restTemplate
-                .exchange("/candidates/9999", HttpMethod.PATCH, request, String.class);
-
-        assertThat(updateResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-        assertThat(updateResponse.getBody()).isEqualTo("Could not find candidate 9999");
+        mockMvc.perform(patch("/candidates/{id}", "9999")
+                                .header("Content-Type", "application/json")
+                                .content(updateJson))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+                .andExpect(content().string("Could not find candidate 9999"));
     }
 }
--- src/test/java/metthis/voting_system/api/TestUtils.java
+++ src/test/java/metthis/voting_system/api/TestUtils.java
@@ -1,13 +1,37 @@
 package metthis.voting_system.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import java.io.IOException;
 
 public class TestUtils {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     byte[] fileToBytes(String fileName) throws IOException {
         return this.getClass().getResourceAsStream(fileName).readAllBytes();
     }
 
     String fileToString(String fileName) throws IOException {
         return new String(fileToBytes(fileName));
+    }
+
+
+    // Following overloads differ only by the type of newValue, Object can't be used as a type
+    String modifyJsonField(String json, String field, String newValue)
+            throws JsonProcessingException {
+        JsonNode jsonNode = objectMapper.readTree(json);
+        ((ObjectNode) jsonNode).put(field, newValue);
+        return objectMapper.writeValueAsString(jsonNode);
+    }
+
+    String modifyJsonField(String json, String field, Boolean newValue)
+            throws JsonProcessingException {
+        JsonNode jsonNode = objectMapper.readTree(json);
+        ((ObjectNode) jsonNode).put(field, newValue);
+        return objectMapper.writeValueAsString(jsonNode);
     }
 }
