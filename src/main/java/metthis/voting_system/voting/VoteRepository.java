e6d2b55d072cf581dae4e0228aac0c862ea29092
metthis <matyas.vilimek@gmail.com>
2024-01-14 12:17:57 +0200

    VoteRepository now extends ListCrudRepository instead of CrudRepository

--- src/main/java/metthis/voting_system/voting/VoteRepository.java
+++ src/main/java/metthis/voting_system/voting/VoteRepository.java
@@ -1,10 +1,10 @@
 package metthis.voting_system.voting;
 
+import org.springframework.data.repository.ListCrudRepository;
+
 import java.util.UUID;
 
-import org.springframework.data.repository.CrudRepository;
-
 public interface VoteRepository
-        extends CrudRepository<Vote, UUID> {
+        extends ListCrudRepository<Vote, UUID> {
 
 }
