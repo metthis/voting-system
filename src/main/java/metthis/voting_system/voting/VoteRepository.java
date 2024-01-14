298dcc734ca4fc0d374dcf8ce21963d583e2cdfd
metthis <matyas.vilimek@gmail.com>
2024-01-14 14:16:33 +0200

    VoteRepository now also extends ListPagingAndSortingRepository

--- src/main/java/metthis/voting_system/voting/VoteRepository.java
+++ src/main/java/metthis/voting_system/voting/VoteRepository.java
@@ -1,10 +1,11 @@
 package metthis.voting_system.voting;
 
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
 
 import java.util.UUID;
 
 public interface VoteRepository
-        extends ListCrudRepository<Vote, UUID> {
+        extends ListCrudRepository<Vote, UUID>, ListPagingAndSortingRepository<Vote, UUID> {
 
 }
