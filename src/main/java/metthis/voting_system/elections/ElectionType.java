22b47f609ee134c7ceb79225c4f47dc25e2b313f
metthis <matyas.vilimek@gmail.com>
2024-01-20 15:44:07 +0200

    add ElectionType with a test

---
+++ src/main/java/metthis/voting_system/elections/ElectionType.java
@@ -0,0 +1,5 @@
+package metthis.voting_system.elections;
+
+public enum ElectionType {
+    PRESIDENTIAL_ELECTION;
+}
---
+++ src/test/java/metthis/voting_system/elections/ElectionTypeTests.java
@@ -0,0 +1,11 @@
+package metthis.voting_system.elections;
+
+import org.junit.jupiter.api.Test;
+
+public class ElectionTypeTests {
+
+    @Test
+    void presidentialElectionExists() {
+        ElectionType type = ElectionType.PRESIDENTIAL_ELECTION;
+    }
+}
